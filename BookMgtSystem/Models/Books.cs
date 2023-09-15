efa5e8595411423790c0483b886911fdb3eeb765
Leah <lelianagichuhi67@gmail.com>
2023-09-15 16:39:00 +0300

    initial

---
+++ BookMgtSystem/Models/Books.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookMgtSystem.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public string Author { get; set; } = string.Empty;
+
+    }
+}
